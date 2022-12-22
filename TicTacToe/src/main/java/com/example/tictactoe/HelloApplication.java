e7c6d4eaa857b181c24be6b6db7fae5d132ea477
project <yana_nacheva00@abv.bg>
2022-12-22 09:37:58 +0200

    Add tic tac toe project

---
+++ TicTacToe/src/main/java/com/example/tictactoe/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+    public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
